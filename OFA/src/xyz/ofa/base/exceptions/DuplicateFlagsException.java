ec73e0e688c8f7de1c533b737a5837da0d79024a
EpicZerg <kenny@kropp.pro>
2016-07-28 19:10:04 +0200

    removed uneccessary classes

--- OFA/src/xyz/ofa/base/exceptions/DuplicateFlagsException.java
+++
@@ -1,7 +0,0 @@
-package xyz.ofa.base.exceptions;
-
-public class DuplicateFlagsException extends Exception {
-	public DuplicateFlagsException(){
-		super("Duplicated Flags in the registered Flags");
-	}
-}
